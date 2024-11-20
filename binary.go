a2eb0831442fcfcca592e3bbab47e3f1e825ef79
Willy-Willy7 <185855937+Willy-Willy7@users.noreply.github.com>
2024-11-21 00:00:00 +0500

    'add file binary.go'

---
+++ binary.go
@@ -0,0 +1,16 @@
+package main
+
+func BinarySearch(arr []int, target int) int {
+	low, high := 0, len(arr)-1
+	for low <= high {
+		mid := (low + high) / 2
+		if arr[mid] == target {
+			return mid
+		} else if arr[mid] < target {
+			low = mid + 1
+		} else {
+
+		}
+	}
+	return -1
+}
